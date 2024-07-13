8ddbc75f2d79a5251ba406bc6684c995b6cd15a3
ParthBadgujar <nachiketbadgujar0901@gmail.com>
2024-07-13 16:41:50 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,233 @@
+<?php
+
+include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<div class="heading">
+   <h3>about us</h3>
+   <p> <a href="home.php">Home</a> / About </p>
+</div>
+
+<section class="about">
+
+   <div class="flex">
+
+      <div class="image">
+         <img src="images/Home1.jpg" alt="">
+      </div>
+
+      <div class="content">
+         <h3>why choose us?</h3>
+         <p>"Our website "The Last Chapter" is an online book store which features books from every literary genre, allowing us to appeal to any particular taste a customer may have. We offer a tremendous gathering of books in the various classifications of Fiction, Non-fiction, Biographies, History, Religions, Self – Help, and Children.</p>
+         <p>Can't find what you're looking for? We can order the book for you!</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+   </div>
+
+</section>
+ <!--
+<section class="reviews">
+
+   <h1 class="title">client's reviews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/pic-1.png" alt="">
+         <p>Great reading website! The collection of books is extensive with discounts almost always available. It's highly recommended website. Loved it!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>John Deo</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-2.png" alt="">
+         <p>The website is amazing, users can get to know different kinds of books that they were unaware of by just searching using keywords.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+         </div>
+         <h3>Erick Martinez</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-3.png" alt="">
+         <p>This website is very convenient and easy to use. Has a user friendly interface. A perfect platform to buy and also sell books.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+            <i class="fas fa-star-half-alt"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Robin Walker</h3>
+      </div>
+
+     <div class="box">
+         <img src="images/pic-4.png" alt="">
+         <p>Fantastic website!!! Bought some books at very discounted prices and they arrived in excellent condition. Excellent customer service.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Martin Yancey</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-5.png" alt="">
+         <p>Fastest and hassle free delivery! The book quality is amazing and highly pocket friendly!! </p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Stephen Metcalfe</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-6.png" alt="">
+         <p>Had a really great experience on ordering from this website. The packaging was great, no compromises on quality. Looking forward to buying more books!!</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+         </div>
+         <h3>Alen Peter</h3>
+      </div>
+
+   </div>
+
+</section>
+-->
+
+
+<!--
+<section class="authors">
+
+   <h1 class="title">Authors</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/author-1.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Henry Fielding</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-2.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Emilie Zola</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-3.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Thomas Hardy</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-4.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Jane Austen</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-5.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>George Elliot</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/author-6.jpg" alt="">
+         <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+         </div>
+         <h3>Charlotte Bronte</h3>
+      </div>
+
+   </div>
+
+   -->
+
+</section>
+
+<?php include 'footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+     <link rel ="https://getbootstrap.com/">
+ </head>
+ </html>
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box" >
+         <i><h3>Quick Links</h3></i>
+         <!--<a href="home.php">Home</a>
+         <a href="about.php">About</a>
+         <a href="shop.php">Shop</a>
+         <a href="contact.php">Contact</a>-->
+         <a target="_blank" href="home.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Home</a></li>
+         <a target="_blank" href="about.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>About</a></li>
+         <a target="_blank" href="shop.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Shop</a></li>
+         <a target="_blank" href="contact.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Contact</a></li>
+      </div>
+
+      <div class="box">
+         <i><h3>Extra Links</h3></i>
+         <!--<a href="login.php">Login</a>
+         <a href="register.php">Register</a>
+         <a href="cart.php">Cart</a>
+         <a href="orders.php">Orders</a>-->
+         <a target="_blank" href="login.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Login</a></li>
+         <a target="_blank" href="register.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Register</a></li>
+         <a target="_blank" href="cart.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Cart</a></li>
+         <a target="_blank" href="orders.php"><i class="fa fa-angle-double-right" aria-hidden="true"></i>Orders</a></li>
+      </div>
+
+      <div class="box">
+         <i><h3>Contact Info</h3></i>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> thenextchapter@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Pune, India - 400112 </p>
+      </div>
+
+      <div class="box">
+         <i><h3>Follow Us</h3></i>
+         <a href="https://www.facebook.com/"> <i class="fab fa-facebook-f"></i> Facebook </a>
+         <a href="https://twitter.com/"> <i class="fab fa-twitter"></i> Twitter </a>
+         <a href="https://www.instagram.com/"> <i class="fab fa-instagram"></i> Instagram </a>
+         <a href="https://in.linkedin.com/"> <i class="fab fa-linkedin"></i> Linkedin </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> &copy; copyright  @ <?php echo date('Y'); ?> by <span>AISSMS COE</span> </p>
+
+</section>
